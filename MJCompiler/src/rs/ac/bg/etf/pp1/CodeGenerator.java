4aa3c1f2613bfb59ca19cddb9f4b8dddec3b230b
jelena3214 <jelena.cvetic3214@gmail.com>
2024-01-22 22:36:17 +0100

    Added designator, method, return, constants and expr visits in CodeGenerator class

--- MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,13 +1,23 @@
 package rs.ac.bg.etf.pp1;
 
-import javax.print.attribute.standard.PrinterState;
 
 import rs.ac.bg.etf.pp1.ast.Addop;
 import rs.ac.bg.etf.pp1.ast.AddopMinus;
 import rs.ac.bg.etf.pp1.ast.AddopPlus;
+import rs.ac.bg.etf.pp1.ast.ArrayDesignatorDecl;
+import rs.ac.bg.etf.pp1.ast.DesignatorBasicDecl;
 import rs.ac.bg.etf.pp1.ast.DesignatorStatementAssign;
 import rs.ac.bg.etf.pp1.ast.DesignatorStatementDec;
 import rs.ac.bg.etf.pp1.ast.DesignatorStatementInc;
+import rs.ac.bg.etf.pp1.ast.ExprDash;
+import rs.ac.bg.etf.pp1.ast.FactorBoolConst;
+import rs.ac.bg.etf.pp1.ast.FactorCharConst;
+import rs.ac.bg.etf.pp1.ast.FactorDesignator;
+import rs.ac.bg.etf.pp1.ast.FactorNew;
+import rs.ac.bg.etf.pp1.ast.FactorNumConst;
+import rs.ac.bg.etf.pp1.ast.MethodDecl;
+import rs.ac.bg.etf.pp1.ast.MethodTypeDecl;
+import rs.ac.bg.etf.pp1.ast.MethodTypeVoid;
 import rs.ac.bg.etf.pp1.ast.Mulop;
 import rs.ac.bg.etf.pp1.ast.MulopDiv;
 import rs.ac.bg.etf.pp1.ast.MulopMod;
@@ -15,7 +25,8 @@ import rs.ac.bg.etf.pp1.ast.MulopMultiply;
 import rs.ac.bg.etf.pp1.ast.PrintStmtNoWidth;
 import rs.ac.bg.etf.pp1.ast.PrintStmtWidth;
 import rs.ac.bg.etf.pp1.ast.ReadStmt;
-import rs.ac.bg.etf.pp1.ast.SyntaxNode;
+import rs.ac.bg.etf.pp1.ast.ReturnStmt;
+import rs.ac.bg.etf.pp1.ast.ReturnStmtExpr;
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.Tab;
@@ -23,7 +34,9 @@ import rs.etf.pp1.symboltable.concepts.Obj;
 import rs.etf.pp1.symboltable.concepts.Struct;
 
 public class CodeGenerator extends VisitorAdaptor {
-
+	
+	private boolean returnFound = false;
+	
 	private int mainPc = -1;
 	
 	public int getMainPc(){
@@ -100,4 +113,128 @@ public class CodeGenerator extends VisitorAdaptor {
 		if(mulop instanceof MulopDiv)Code.put(Code.div);
 		if(mulop instanceof MulopMod)Code.put(Code.rem);
 	}
+	
+	
+	public void visit(FactorNumConst fact) {
+    	int constant = fact.getN1();
+    	Code.loadConst(constant);
+    }
+    
+    public void visit(FactorCharConst fact) {
+    	char constValue = fact.getC1().charAt(0);
+    	int constant = constValue;
+    	Code.loadConst(constant);
+    }
+    
+    public void visit(FactorBoolConst fact) {
+    	boolean constValue = fact.getB1();
+    	
+    	if (constValue == true) {
+    		Code.loadConst(1);
+    	}
+    	else {
+    		Code.loadConst(0);
+    	}
+    }
+    
+    public void visit(FactorDesignator fact) {
+    	Obj ob = fact.getDesignator().obj;
+    	Code.load(ob);
+    }
+    
+    public void visit(FactorNew fact) {
+    	Struct type = fact.struct;
+    	
+    	Code.put(Code.newarray);
+    	if(type == Tab.charType) {
+    		Code.put(0);
+    	}else {
+    		Code.put(1);
+    	}
+    }
+    
+    public void visit(ExprDash expr) {
+    	Code.put(Code.neg);
+    }
+    
+    public void visit(MethodTypeDecl meth) {
+		Obj func = meth.obj;
+		String funcName = func.getName();
+		
+		func.setAdr(Code.pc);
+		
+		if ("main".equals(funcName)) {
+			mainPc = Code.pc;
+		}
+		
+		int argCnt = func.getLevel();
+		int localAndArgCnt = func.getLocalSymbols().size();
+		
+		Code.put(Code.enter);
+		Code.put(argCnt);
+		Code.put(localAndArgCnt);
+    }
+    
+    public void visit(MethodTypeVoid meth) {
+		Obj func = meth.obj;
+		String funcName = func.getName();
+		
+		func.setAdr(Code.pc);
+		
+		if ("main".equals(funcName)) {
+			mainPc = Code.pc;
+		}
+		
+		int argCnt = func.getLevel();
+		int localAndArgCnt = func.getLocalSymbols().size();
+		
+		Code.put(Code.enter);
+		Code.put(argCnt);
+		Code.put(localAndArgCnt);
+    }
+    
+	public void visit(MethodDecl methodDecl){
+		Struct methStruct = methodDecl.obj.getType();
+		
+		// fali return
+		if (methStruct != Tab.noType && !returnFound) {
+			Code.put(Code.trap);
+			Code.put(1);
+		}
+		else if (methStruct == Tab.noType && !returnFound) {
+			Code.put(Code.exit);
+			Code.put(Code.return_);
+		}
+		
+		returnFound = false;
+	}
+	
+	public void visit(ReturnStmtExpr returnExpr){
+		returnFound = true;
+		Code.put(Code.exit);
+		Code.put(Code.return_);
+		
+	}
+	
+	public void visit(ReturnStmt returnNoExpr){
+		returnFound = true;
+		Code.put(Code.exit);
+		Code.put(Code.return_);
+	}
+
+	//TODO check
+	public void visit(DesignatorBasicDecl designator){
+		Code.load(designator.obj);
+//		SyntaxNode parent = designator.getParent();
+//		
+//		if(Assignment.class != parent.getClass() && FuncCall.class != parent.getClass() && ProcCall.class != parent.getClass()){
+//			
+//		}
+	}
+	
+	public void visit(ArrayDesignatorDecl arr) {
+		Code.load(arr.getDesignator().obj);
+	}
+
+	
 }
